4e19f317df29d1a0a0edb3fe1962da2612025497
NgThanhHung04 <nguyenthanhhungg04@gmail.com>
2024-05-17 10:06:57 +0700

    gỐC

---
+++ SmartPhone/app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use App\Slide;
+
+
+class PageController extends Controller
+{
+    public function getIndex()
+    {
+        $slide = Slide::all();
+        $new_product = Product::where('id_type', 1)->paginate(8);
+        $sanpham_khuyenmai = Product::where('promotion_price', '<>', 0)->paginate(8);
+   
+      
+        return view('page.trangchu', compact('slide', 'new_product', 'sanpham_khuyenmai'));
+    }
+}
